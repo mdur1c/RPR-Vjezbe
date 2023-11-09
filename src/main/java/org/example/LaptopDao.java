4292632c97c74fb025ea5ae2fcbf3cedb23e8ab6
mduric1 <mduric1@etf.unsa.ba>
2023-11-09 12:54:12 +0100

    lv4

---
+++ src/main/java/org/example/LaptopDao.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface LaptopDao {
+     void dodajLaptopUListu(Laptop laptop);
+     void dodajLaptopUFile(Laptop laptop) throws IOException;
+     void napuniListu(ArrayList<Laptop> laptopi);
+     Laptop getLaptop(String procesor);
+     ArrayList<Laptop> vratiPodatkeIzDatoteke() throws IOException, ClassNotFoundException;
+}
---
+++ src/main/java/org/example/NeodgovarajuciProcesorException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class NeodgovarajuciProcesorException extends RuntimeException{
+    NeodgovarajuciProcesorException(String poruka){
+        super(poruka);
+    }
+}
